4bee119e854801a0be3cdda166db49ca078cf61d
Hinkuok Kong <hinkuok.kong@calix.com>
2018-08-31 22:52:06 +0000

    add missing packages

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ apk update
 apk add git
 # apk add py3-pip
 apk add py3-gobject3
+apk add py3-gevent
 pip3 install xmltodict
 pip3 install texttable
 pip3 install pydbus
+pip3 install bottle
+pip3 install gevent-websocket
+
 
